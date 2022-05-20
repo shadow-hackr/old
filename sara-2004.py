9e52d1a14b108a2b7e2b34f635344d40e2a0be4d
shadow-hackr <88612340+shadow-hackr@users.noreply.github.com>
2022-05-21 00:28:12 +0500

    Update sara-2004.py

--- sara-2004.py
+++ sara-2004.py
@@ -138,7 +138,7 @@ $$\   $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r\r %s[>_] [MR.RAKIB.404] : %s/%s -> \033[0;92m [ txt-OK:%s ]- \033[0;91m[txt-CP:%s ]"%(B,self.loop, len(self.id), len(self.ok), len(self.cp))
+			"\r\r %s[>_] [SARA X SHADOW] : %s/%s -> \033[0;92m [ txt-OK:%s ]- \033[0;91m[txt-CP:%s ]"%(B,self.loop, len(self.id), len(self.ok), len(self.cp))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
